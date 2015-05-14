eb2398a39d76352e47a232013d0cd621f71152d1
Oscar <Oscar@Oscar-PC>
2015-05-14 21:11:58 +0200

    Merge origin/master
    Conflicts:
    	src/com/morethansimplycode/management/DataManagement.java
    	src/com/morethansimplycode/management/DataManagementUtil.java

--- src/com/morethansimplycode/management/DataManagementUtil.java
+++ src/com/morethansimplycode/management/DataManagementUtil.java
@@ -6,15 +6,7 @@
 package com.morethansimplycode.management;
 
 import com.morethansimplycode.data.Data;
-import java.sql.Connection;
-import java.sql.DriverManager;
-import java.sql.ResultSet;
-import java.sql.SQLException;
-import java.sql.Statement;
-import java.time.LocalDate;
 import java.util.ArrayList;
-import java.util.logging.Level;
-import java.util.logging.Logger;
 
 /**
  *
